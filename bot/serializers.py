8f07754c15ca60a41ff18288a785bff0ae9134a9
arjuna <amanitocornholio@proton.me>
2023-08-16 01:14:16 -0300

    doc

---
+++ bot/serializers.py
@@ -0,0 +1,21 @@
+
+from rest_framework import serializers
+from core.models import User
+from bot.models import TgUser
+
+
+class TgUserVerificationSerializer(serializers.ModelSerializer):
+
+    def validate_verification_code(self, verification_code: str) -> str:
+        """Проверяет наличие пользователя
+        с отправленным верификационным кодом в базе данных"""
+        if not TgUser.objects.filter(
+                verification_code=verification_code).exists():
+            raise serializers.ValidationError("Код неверный")
+        return verification_code
+
+    class Meta:
+        id = serializers.IntegerField(read_only=True)
+        model = User
+        fields = ['verification_code']
+
